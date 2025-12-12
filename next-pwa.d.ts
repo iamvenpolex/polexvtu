962ef004b31dcb43610467e88171e592f1eaa6d0
Adejorin Ayomiposi <adejorinayomiposi@gmail.com>
2025-12-13 00:52:15 +0100

    third commit

--- next-pwa.d.ts
+++ next-pwa.d.ts
@@ -1,15 +1,26 @@
-// next-pwa.d.ts
 declare module 'next-pwa' {
   import type { NextConfig } from 'next';
+
+  interface RuntimeCachingOptions {
+    urlPattern: RegExp;
+    handler: string;
+    options?: {
+      cacheName?: string;
+      expiration?: {
+        maxEntries?: number;
+        maxAgeSeconds?: number;
+      };
+    };
+  }
 
   interface PWAConfig {
     dest?: string;
     register?: boolean;
     skipWaiting?: boolean;
     disable?: boolean;
+    runtimeCaching?: RuntimeCachingOptions[];
   }
 
-  // Allow NextConfig combined with PWA options
   function withPWA(config: NextConfig & PWAConfig): NextConfig;
 
   export default withPWA;
